44866884dc020291e6bb29c8f12fd31639cd0134
ElodinLaarz <caleblgeiger@gmail.com>
2025-05-10 08:48:55 -0400

    and then we made the errors make sense

--- lesson-1/main.cpp
+++ lesson-1/main.cpp
@@ -2,59 +2,72 @@
 #include <iostream>
 #include <limits>
 
-enum class ErrorCode {
-	InvalidInput,
+enum class DoublingError {
 	Overflow,
 	Underflow,
 };
 
-std::expected<int, ErrorCode> doubleInt(int input) {
+void printDoublingError(int input, DoublingError error) {
+	switch (error) {
+	case DoublingError::Overflow:
+		std::cout << "Overflow occurred while doubling the integer " << input << std::endl;
+		break;
+	case DoublingError::Underflow:
+		std::cout << "Underflow occurred while doubling the integer" << input << std::endl;
+		break;
+	}
+};
+
+enum class ParsingError {
+	NotAnInteger,
+	InvalidTrailingCharacters,
+};
+
+void printParsingError(ParsingError error) {
+	switch (error) {
+	case ParsingError::NotAnInteger:
+		std::cout << "Input is not a valid integer." << std::endl;
+		break;
+	case ParsingError::InvalidTrailingCharacters:
+		std::cout << "Trailing characters detected after integer input." << std::endl;
+		break;
+	}
+};
+
+void invalidInputError() {
+	switch (std::cin.fail()) {
+	case true:
+		printParsingError(ParsingError::NotAnInteger);
+		break;
+	case false:
+		printParsingError(ParsingError::InvalidTrailingCharacters);
+		break;
+	}
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+std::expected<int, DoublingError> doubleInt(int input) {
 	if (input > (std::numeric_limits<int>::max() / 2)) {
-		return std::unexpected(ErrorCode::Overflow);
+		return std::unexpected(DoublingError::Overflow);
 	}
 	if (input < (std::numeric_limits<int>::min() / 2)) {
-		return std::unexpected(ErrorCode::Underflow);
+		return std::unexpected(DoublingError::Underflow);
 	}
 	return input * 2;
-}
-
-void printError(ErrorCode error) {
-	switch (error) {
-		case ErrorCode::InvalidInput:
-			std::cout << "Invalid input.\n";
-			break;
-		case ErrorCode::Overflow:
-			std::cout << "Overflow occurred.\n";
-			break;
-		case ErrorCode::Underflow:
-			std::cout << "Underflow occurred.\n";
-			break;
-	}
-}
-
-void printInvalidationReason() {
-	printError(ErrorCode::InvalidInput);
-	if (!std::cin.fail()) {
-		std::cout << "Trailing characters detected after integer input.\n";
-	} 
 }
 
 int main() {
 	int inputNumber{ };
 	std::cout << "Please enter an integer: ";
 	while (!(std::cin >> inputNumber) || std::cin.peek() != '\n') {
-		printInvalidationReason();
-		std::cin.clear();
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		invalidInputError();
 		std::cout << "Please enter a valid integer: ";
 	}
-	// Even in the success case, good practice to clear the rest of the
-	// line to ignore any remaining characters.
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	std::cout << "You entered: " << inputNumber << std::endl;
-	std::expected<int, ErrorCode> result{doubleInt(inputNumber)};
+	std::expected<int, DoublingError> result{doubleInt(inputNumber)};
 	if (!result.has_value()) {
-		printError(result.error());
+		printDoublingError(inputNumber, result.error());
 		return 1;
 	} 
 	std::cout << "Double of the input is: " << result.value() << std::endl;
